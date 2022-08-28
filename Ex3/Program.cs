e79b6920bf4a1617e3224a20d5f300e56986a032
DmitriySolenkov <soledimk@gmail.com>
2022-08-28 17:52:11 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,51 @@
+﻿int[,] FillMatrix(int length1, int length2)
+{
+    int[,] array = new int[length1, length2];
+    for (int i = 0; i < length1; i++)
+    {
+        for (int j = 0; j < length2; j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return array;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            Console.Write($"{matrix[k, l]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillMatrix(Prompt("Введите число строк: "), Prompt("Введите число колонок: "));
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            sum += matrix[i, j];
+        }
+    }
+}
+
+Console.WriteLine($"Сумма значений главной диагонали: {sum}");
